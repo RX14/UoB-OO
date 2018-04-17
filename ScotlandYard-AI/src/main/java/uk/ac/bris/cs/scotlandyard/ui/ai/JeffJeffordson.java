5477a93cfa65ef088446333a535fe21f7c56a177
RX14 <chris@rx14.co.uk>
2018-04-17 16:55:42 +0100

    Name Jeff

--- ScotlandYard-AI/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/JeffJeffordson.java
+++ ScotlandYard-AI/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/JeffJeffordson.java
@@ -1,19 +1,16 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
-import java.util.*;
-import java.util.function.Consumer;
-
-import org.omg.PortableInterceptor.INACTIVE;
-import sun.font.CompositeGlyphMapper;
 import uk.ac.bris.cs.gamekit.graph.Graph;
 import uk.ac.bris.cs.gamekit.graph.Node;
 import uk.ac.bris.cs.scotlandyard.ai.ManagedAI;
 import uk.ac.bris.cs.scotlandyard.ai.PlayerFactory;
 import uk.ac.bris.cs.scotlandyard.model.*;
 
-// TODO name the AI
+import java.util.*;
+import java.util.function.Consumer;
+
 @ManagedAI("Jeff")
-public class MyAI implements PlayerFactory {
+public class JeffJeffordson implements PlayerFactory {
 
     @Override
     public Player createPlayer(Colour colour) {
@@ -24,7 +21,7 @@ public class MyAI implements PlayerFactory {
         private final Colour colour;
         private final Random random = new Random();
 
-        public MyPlayer(Colour colour) {
+        MyPlayer(Colour colour) {
             this.colour = colour;
             if (colour != Colour.BLACK) throw new IllegalArgumentException("The AI is for MrX only");
         }
@@ -36,7 +33,7 @@ public class MyAI implements PlayerFactory {
         }
 
         private int distanceBetweenLocations(Graph<Integer, Transport> graph, int from, int to) {
-            Map<Node<Integer>,Integer> distances = new HashMap<>();
+            Map<Node<Integer>, Integer> distances = new HashMap<>();
             PriorityQueue<Node<Integer>> nodes = new PriorityQueue<>();
 
             distances.put(graph.getNode(from), 0);
