331fdcc8d0479b976051fbd89847b4f789edade4
eskimo801 <lukemp007@gmail.com>
2018-04-17 16:39:54 +0100

    Did a bit of AI

--- ScotlandYard-AI/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAI.java
+++ ScotlandYard-AI/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAI.java
@@ -1,44 +1,49 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Consumer;
 
+import org.omg.PortableInterceptor.INACTIVE;
+import sun.font.CompositeGlyphMapper;
+import uk.ac.bris.cs.gamekit.graph.Graph;
+import uk.ac.bris.cs.gamekit.graph.Node;
 import uk.ac.bris.cs.scotlandyard.ai.ManagedAI;
 import uk.ac.bris.cs.scotlandyard.ai.PlayerFactory;
-import uk.ac.bris.cs.scotlandyard.model.Colour;
-import uk.ac.bris.cs.scotlandyard.model.Move;
-import uk.ac.bris.cs.scotlandyard.model.Player;
-import uk.ac.bris.cs.scotlandyard.model.ScotlandYardView;
+import uk.ac.bris.cs.scotlandyard.model.*;
 
 // TODO name the AI
 @ManagedAI("Jeff")
 public class MyAI implements PlayerFactory {
 
-	// TODO create a new player here
-	@Override
-	public Player createPlayer(Colour colour) {
-		return new MyPlayer(colour);
-	}
+    @Override
+    public Player createPlayer(Colour colour) {
+        return new MyPlayer(colour);
+    }
 
-	// TODO A sample player that selects a random move
-	private static class MyPlayer implements Player {
-		private final Colour colour;
-		private final Random random = new Random();
+    private static class MyPlayer implements Player {
+        private final Colour colour;
+        private final Random random = new Random();
 
-		public MyPlayer(Colour colour) {
-			this.colour = colour;
-			if (colour != Colour.BLACK) throw new IllegalArgumentException("The AI is for MrX only");
-		}
+        public MyPlayer(Colour colour) {
+            this.colour = colour;
+            if (colour != Colour.BLACK) throw new IllegalArgumentException("The AI is for MrX only");
+        }
 
-		@Override
-		public void makeMove(ScotlandYardView view, int location, Set<Move> moves,
-				Consumer<Move> callback) {
-			// TODO do something interesting here; find the best move
-			// picks a random move
-			callback.accept(new ArrayList<>(moves).get(random.nextInt(moves.size())));
+        @Override
+        public void makeMove(ScotlandYardView view, int location, Set<Move> moves,
+                             Consumer<Move> callback) {
 
-		}
-	}
+        }
+
+        private int distanceBetweenLocations(Graph<Integer, Transport> graph, int from, int to) {
+            Map<Node<Integer>,Integer> distances = new HashMap<>();
+            PriorityQueue<Node<Integer>> nodes = new PriorityQueue<>();
+
+            distances.put(graph.getNode(from), 0);
+
+            //graph.getEdgesFrom(graph.getNode(from));
+
+            return 0;
+        }
+    }
 }
