08e424e79d8890ea6be17b5f7b8041ac60110f03
RX14 <chris@rx14.co.uk>
2018-04-18 13:42:28 +0100

    Add simple AI

--- ScotlandYard-AI/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/JeffJeffordson.java
+++ ScotlandYard-AI/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/JeffJeffordson.java
@@ -1,13 +1,15 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
-import uk.ac.bris.cs.gamekit.graph.Graph;
-import uk.ac.bris.cs.gamekit.graph.Node;
 import uk.ac.bris.cs.scotlandyard.ai.ManagedAI;
 import uk.ac.bris.cs.scotlandyard.ai.PlayerFactory;
 import uk.ac.bris.cs.scotlandyard.model.*;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 @ManagedAI("Jeff")
 public class JeffJeffordson implements PlayerFactory {
@@ -27,7 +29,28 @@ public class JeffJeffordson implements PlayerFactory {
 
         @Override
         public void makeMove(ScotlandYardView view, int location, Set<Move> moves, Consumer<Move> callback) {
+            List<Integer> detectiveLocations = view.getPlayers().stream()
+                    .filter(colour -> colour != Colour.BLACK)
+                    .map(colour -> view.getPlayerLocation(colour).get())
+                    .collect(Collectors.toList());
 
+            Map<Integer, Integer> distances = Utils.generateDistances(view.getGraph(), detectiveLocations);
+
+            Move moveToMake = moves.stream().max(Comparator.comparingInt(o -> distances.get(getDestination(location, o)))).get();
+
+            callback.accept(moveToMake);
+        }
+
+        private Integer getDestination(int location, Move move) {
+            if (move instanceof PassMove) {
+                return location;
+            } else if (move instanceof TicketMove) {
+                return ((TicketMove) move).destination();
+            } else if (move instanceof DoubleMove) {
+                return ((DoubleMove) move).finalDestination();
+            } else {
+                throw new IllegalStateException("Unknown move type");
+            }
         }
     }
 }
--- ScotlandYard-AI/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Utils.java
+++ ScotlandYard-AI/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Utils.java
@@ -4,12 +4,40 @@ import uk.ac.bris.cs.gamekit.graph.Graph;
 import uk.ac.bris.cs.gamekit.graph.Node;
 import uk.ac.bris.cs.scotlandyard.model.Transport;
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.PriorityQueue;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Utils {
+    public static Map<Integer, Integer> generateDistances(Graph<Integer, Transport> graph, List<Integer> startingLocations) {
+        List<NodeWrapper> startingNodes = startingLocations.stream()
+                .map(number -> new NodeWrapper(graph.getNode(number), 0))
+                .collect(Collectors.toList());
+
+        Map<Integer, Integer> nodeDistances = new HashMap<>();
+        PriorityQueue<NodeWrapper> unmarkedNodes = new PriorityQueue<>(startingNodes);
+
+        while (!unmarkedNodes.isEmpty()) {
+            // Take the lowest-distance potentially-unmarked node
+            NodeWrapper node = unmarkedNodes.poll();
+
+            // If we've already marked the node, ignore it
+            if (nodeDistances.containsKey(node.node.value())) continue;
+
+            // Mark the node
+            nodeDistances.put(node.node.value(), node.distance);
+
+            // Add it's neighbours to the list of unmarked nodes
+            graph.getEdgesFrom(node.node).forEach(edge -> {
+                if (!nodeDistances.containsKey(edge.destination().value())) {
+                    int newDistance = node.distance + 1;
+                    unmarkedNodes.add(new NodeWrapper(edge.destination(), newDistance));
+                }
+            });
+        }
+
+        return nodeDistances;
+    }
+
     public static int distanceBetween(Graph<Integer, Transport> graph, int from, int to) {
         Set<Node<Integer>> markedNodes = new HashSet<>();
         PriorityQueue<NodeWrapper> unmarkedNodes = new PriorityQueue<>();
@@ -35,7 +63,7 @@ public class Utils {
 
     private static class NodeWrapper implements Comparable<NodeWrapper> {
         final Node<Integer> node;
-        int distance;
+        final int distance;
 
         NodeWrapper(Node<Integer> node, int distance) {
             this.node = node;
